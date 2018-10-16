9cd401eb5acd4159b9ed0493d4a5af196a2eeb65
v3rba <verbush@gmail.com>
2018-10-16 15:41:53 +0300

    Display products, step 1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181015131548) do
+ActiveRecord::Schema.define(version: 20181015134114) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
