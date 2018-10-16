c0c944d208284514c312c111c2c1f91a4e6c187a
v3rba <verbush@gmail.com>
2018-10-16 17:04:07 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,17 +1,19 @@
 function something()
 {
+  var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-  var x = window.localStorage.getItem('aaa');
+  x = x * 1 + 1; // x = x + 1
 
-  x = x * 1 + 1;
-
-  window.localStorage.setItem('aaa', x);
+  window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
   alert(x);
-
 }
 
 function add_to_cart(id)
 {
-  alert('You added pizza with id: ' + id);
+  var key = 'product_' + id;
+
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
 }
