4f44764046e11fe6001b2de95b72a98e439bfc93
v3rba <verbush@gmail.com>
2018-10-18 12:19:36 +0300

    Parse orders input

--- app.rb
+++ app.rb
@@ -18,10 +18,33 @@ get '/about' do
   erb :about
 end
 
-get '/cart' do
-  erb :cart
+post '/cart' do
+  orders_input = params[:orders]
+  @orders = parse_orders_input orders_input
+
+  erb "Hello! #{@orders.inspect}"
 end
 
-post '/cart' do
-  erb "Hello!"
+def parse_orders_input(orders_input)
+
+    s1 = orders_input.split(/,/)
+
+    arr = []
+    
+    s1.each do |x|
+      s2 = x.split(/\=/)
+    
+      s3 = s2[0].split(/_/)
+    
+      id = s3[1]
+      cnt = s2[1]
+
+      arr2 = [id, cnt]
+    
+      arr.push arr2
+    
+    end
+
+    return arr
+
 end
